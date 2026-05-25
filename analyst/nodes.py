f8bce6f7e784f68408e61b4b4f6415145f2cfb80
Youssef Tarek <yussef0212@gmail.com>
2026-05-25 15:11:45 +0300

    prompt fix

--- analyst/nodes.py
+++ analyst/nodes.py
@@ -24,7 +24,7 @@ def analyst_call(state: dict, config: RunnableConfig):
                         - The analysis will be in the following sections:
                          - Description of the data: number of rows and columns, along with brief description of the data types, pointing out the label column
                          - Discuss data imbalance if there is, showing the labelled image returned from the tool its address is "app/static/label_pie_chart.png", if the data is not imbalanced, just mention it briefly
-                        - Mention the number of nulls in each column, pointing out columns where the nulls are larger than usual, showing the labelled image returned from the tool its address is "app/static/null_values_bar_chart.png"
+                        - Mention the number of nulls in each column, pointing out columns where the nulls are larger than usual, showing the labelled image returned from the tool its address is "app/static/null_values_bar_chart.png" (if there are no null values, do not add the bar chart for it)
                          - Show the correlation matrix image and discuss it, pointing out patterns its address is "app/static/correlation_matrix.png", if the correlation matrix is not showing any patterns, just mention it briefly
                         """
         )
